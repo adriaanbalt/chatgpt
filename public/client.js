eee9fc40dbc7e9e6e4751114f3c6ed0135140c2a
Adriaan Balt <adriaanbalt@gmail.com>
2022-12-31 17:10:53 +0100

    merging the correct html and js

--- public/client.js
+++ public/client.js
@@ -3,23 +3,38 @@ const responseDiv = document.getElementById('response');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-
+  loader.classList.remove('hidden');
+  
   const prompt = document.getElementById('prompt').value;
   const url = `/completion`;
   const options = {
     method: 'POST',
     headers: {
-        'Content-Type': 'application/json',
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-        prompt: prompt,
+      prompt: prompt,
     }),
   };
   fetch(url,options)
-    .then((res) => res.text())
-    .then((text) => {
+  .then((res) => res.text())
+  .then((text) => {
+      // reveal the loader
+      loader.classList.add('hidden');
       const res = JSON.parse(text);
-      responseText.textContent = res.text;
-      responseImage.src = res.image;
+      
+      // clone the template element
+      const element = template.content.cloneNode(true);
+      // select the element in the template where the data should be inserted
+      element.innerHTML = `
+        <div class="listItem">
+          <h3 id="prompt">Prompt: ${prompt}</h3>
+          <p id="text">Response: ${res.text}</p>
+          <p id="text">Image representation:</p>
+          <img class="image" src="${res.image}"></img>
+        </div>
+      `;
+      // insert the cloned element into the list
+      list.insertAdjacentHTML('afterbegin', element.innerHTML);
     });
 });
